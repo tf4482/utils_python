dbab9f49117da021938856e3501293f07d6eb886
tf4482 <tf4482@gmail.com>
2024-12-14 08:06:50 +0100

    refactor: :recycle: Updated import statement for convert_to_string

--- colored_text.py
+++ colored_text.py
@@ -2,7 +2,8 @@
 
 import sys
 from typing import Dict
-from convert_to_string import convert_to_string
+
+from .convert_to_string import convert_to_string
 
 
 def check_type(input_data):
@@ -31,22 +32,22 @@ def output(color: str, text: str):
         text (str): The text to print.
     """
     color_codes: Dict[str, str] = {
-        "black": '\033[0;30m',
-        "red": '\033[0;31m',
-        "green": '\033[0;32m',
-        "yellow": '\033[0;33m',
-        "blue": '\033[0;34m',
-        "magenta": '\033[0;35m',
-        "cyan": '\033[0;36m',
-        "lgrey": '\033[0;37m',
-        "grey": '\033[1;90m',
-        "lred": '\033[1;91m',
-        "lgreen": '\033[1;92m',
-        "lyellow": '\033[1;93m',
-        "lblue": '\033[1;94m',
-        "lmagenta": '\033[1;95m',
-        "lcyan": '\033[1;96m',
-        "white": '\033[1;97m',
+        "black": "\033[0;30m",
+        "red": "\033[0;31m",
+        "green": "\033[0;32m",
+        "yellow": "\033[0;33m",
+        "blue": "\033[0;34m",
+        "magenta": "\033[0;35m",
+        "cyan": "\033[0;36m",
+        "lgrey": "\033[0;37m",
+        "grey": "\033[1;90m",
+        "lred": "\033[1;91m",
+        "lgreen": "\033[1;92m",
+        "lyellow": "\033[1;93m",
+        "lblue": "\033[1;94m",
+        "lmagenta": "\033[1;95m",
+        "lcyan": "\033[1;96m",
+        "white": "\033[1;97m",
     }
     print(f"{color_codes.get(color, '')}{text}\033[0m")
 
